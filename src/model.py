0c6dd820472994358a998dc6d352283e678c478e
Infaninio <infaninio@gmail.com>
2022-03-16 23:52:34 +0100

    Add ResNet34

--- src/model.py
+++ src/model.py
@@ -1,3 +1,4 @@
+from typing import List, Tuple
 import tensorflow as tf
 
 
@@ -26,3 +27,68 @@ class CNN(tf.keras.Model):
         inputs = self.flatten(inputs)
         inputs = self.dense(inputs)
         return self.predictions(inputs)
+
+class ConvLayer(tf.keras.layers.Layer):
+    def __init__(self, kernel_size: Tuple, filter: int, num_convolutions: int, stride_for_first: int = 1, learnable_skip_connection: bool = False) -> None:
+        super().__init__()
+        self.convolutions: List[tf.keras.layers.Layer] = []
+        self.batch_norms: List[tf.keras.layers.Layer] = []
+        self.activations: List[tf.keras.layers.Layer] = []
+        self.learnable_skip_Connection: tf.keras.Layer = None
+        if stride_for_first > 1:
+            self.convolutions.append(tf.keras.layers.Conv2D(filters=filter, kernel_size=kernel_size, strides=(stride_for_first, stride_for_first), padding='same'))          
+            self.batch_norms.append(tf.keras.layers.BatchNormalization())
+            self.activations.append(tf.keras.layers.ReLU())
+            num_convolutions -= 1
+            if learnable_skip_connection:
+                self.learnable_skip_Connection = tf.keras.layers.Conv2D(filters=filter, kernel_size=(1, 1), strides=(stride_for_first, stride_for_first))
+            else:
+                # TODO non learnable Skip Connection
+                self.learnable_skip_Connection = tf.keras.layers.Conv2D(filters=filter, kernel_size=(1, 1), strides=(stride_for_first, stride_for_first))
+        
+        for _ in range(num_convolutions):
+            self.batch_norms.append(tf.keras.layers.BatchNormalization())
+            self.activations.append(tf.keras.layers.ReLU())
+            self.convolutions.append(tf.keras.layers.Conv2D(filters=filter, kernel_size=kernel_size, padding='same'))
+
+    def call(self, inputs: tf.Tensor, **kwargs):
+        skipValue = inputs
+        x = inputs
+
+        for cnt, layer in enumerate(self.convolutions):
+            x = layer(x)
+            x = self.batch_norms[cnt](x)
+            if not cnt % 2 and cnt != 0:
+                if cnt == 2 and self.learnable_skip_Connection:
+                    x = x + self.learnable_skip_Connection(skipValue)
+                else:
+                    x = x + skipValue
+                skipValue = x
+            x = self.activations[cnt](x)
+        return x
+
+
+class ResNet34(tf.keras.Model):
+    def __init__(self, num_classes: int, **kwargs) -> None:
+        super().__init__(**kwargs)
+        self.conv1 = tf.keras.layers.Conv2D(filters=64, kernel_size=(7, 7), strides=(2, 2))
+        self.avg_pooling_start = tf.keras.layers.AveragePooling2D()
+        self.conv_block1 = ConvLayer((3,3), 64, 6)
+        self.conv_block2 = ConvLayer((3,3), 128, 8, 2, True)
+        self.conv_block3 = ConvLayer((3,3), 256, 12, 2, True)
+        self.conv_block4 = ConvLayer((3,3), 512, 6, 2, True)
+        self.avg_pooling_end = tf.keras.layers.AveragePooling2D()
+        self.flatten = tf.keras.layers.Flatten()
+        self.dense = tf.keras.layers.Dense(num_classes,  activation='softmax')
+    
+    def call(self, inputs: tf.Tensor, **kwargs):
+        x = self.conv1(inputs)
+        x = tf.keras.layers.ReLU()(x)
+        x = self.avg_pooling_start(x)
+        x = self.conv_block1(x)
+        x = self.conv_block2(x)
+        x = self.conv_block3(x)
+        x = self.conv_block4(x)
+        x = self.avg_pooling_end(x)
+        x = self.flatten(x)
+        return self.dense(x)
--- train.py
+++ train.py
@@ -1,18 +1,22 @@
+import os
 import hydra
 from omegaconf import DictConfig
 from src.load_dataset import load_dataset, DatasetType
 import tensorflow as tf
+import src.model
 
 @hydra.main(config_path="./", config_name="train.yaml")
 def train(cfg: DictConfig):
-    model: tf.keras.Model = hydra.utils.instantiate(cfg.model)
-    train_data = load_dataset(cfg.dataset_path, type=DatasetType.TRAIN, batch_size=32, image_size=(132, 132))
-    print(train_data.class_names)
+    
+    train_data = load_dataset(cfg.dataset_path, type=DatasetType.TRAIN, batch_size=cfg.batch_size, image_size=tuple(cfg.image_size))
+    validation_data = load_dataset(cfg.dataset_path, type=DatasetType.VALIDATION, batch_size=cfg.batch_size, image_size=tuple(cfg.image_size))
     metrics = [tf.keras.metrics.SparseCategoricalAccuracy()]
 
+    model: tf.keras.Model = hydra.utils.instantiate(cfg.model)
     model.compile(optimizer=tf.keras.optimizers.Adam(), loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False), metrics=metrics)
-    model.fit(train_data, epochs=6)
+    model.fit(x=train_data, validation_data=validation_data, epochs=20, use_multiprocessing=True, workers=8)
 
 
 if __name__ == "__main__":
+    print(os.curdir)
     train()
